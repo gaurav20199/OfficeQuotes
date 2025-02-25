a8a4fb627d469a29a479d89bc2b16ed835cc00f7
Gaurav <gauravjoshi20199@gmail.com>
2025-02-26 04:31:53 +0530

    Logic for converting user dto to user

--- src/main/java/com/project/officequiz/utils/ConversionUtil.java
+++ src/main/java/com/project/officequiz/utils/ConversionUtil.java
@@ -2,9 +2,10 @@ package com.project.officequiz.utils;
 
 import com.project.officequiz.dto.OptionsDTO;
 import com.project.officequiz.dto.QuoteDTO;
+import com.project.officequiz.dto.UserDTO;
 import com.project.officequiz.entity.Options;
 import com.project.officequiz.entity.Quote;
-
+import com.project.officequiz.entity.User;
 import java.util.List;
 
 public class ConversionUtil {
@@ -15,5 +16,14 @@ public class ConversionUtil {
     }
     public static OptionsDTO convertOptionToOptionDTO(Options option) {
         return new OptionsDTO(option.getOptionText(),option.getOptionOrder());
+    }
+
+    public static User convertUserDTOToUser(UserDTO userDTO,String encodedPassword) {
+        User user = new User();
+        user.setUserName(userDTO.userName());
+        user.setEmail(userDTO.email());
+        user.setPassword(encodedPassword);
+        return user;
+
     }
 }
