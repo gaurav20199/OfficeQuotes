531d823abed7f304ca446c4623178af876c79a1e
Gaurav <gauravjoshi20199@gmail.com>
2025-03-22 15:09:31 +0530

    Migration scripts and dataset for db initilaization

---
+++ src/main/resources/db/migration/V3__Create_Character_Table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE OFFICE_CHARACTER (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    real_name VARCHAR(255) NOT NULL,
+    character_name VARCHAR(255) NOT NULL,
+    number_of_episodes SMALLINT,
+    year_range VARCHAR(255) NOT NULL
+);
